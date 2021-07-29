5fb92ed81e0a0a812eec3a6fb7feb976dc2b505a
Melissa Brewer <melramirez727@gmail.com>
2021-07-29 18:08:23 -0500

    added category model requirements

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,3 +14,4 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
