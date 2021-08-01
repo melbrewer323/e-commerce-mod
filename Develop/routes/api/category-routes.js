0a45080031ca3a4c42c8d1ad71fb9148ebd87dd6
Melissa Brewer <melramirez727@gmail.com>
2021-07-31 21:20:05 -0500

    create a new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,16 +35,22 @@ router.get('/:id', async (req, res) => {
 
 
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
 });
 
 module.exports = router;
